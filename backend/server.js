ef95d949e96e847bc25d436584a0e0f9d2df2a53
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-07-17 11:06:44 +0530

    updated server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/auth.route.js';
 import "./passport/github.auth.js";
 import passport from 'passport';
 import session from 'express-session';
+import path from 'path';
 
 
 dotenv.config();
@@ -15,9 +16,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
@@ -27,7 +28,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
     connectMongoDB();
 });
